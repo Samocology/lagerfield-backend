22161da29987950492a71bc58648688e48def9e9
Samocology <godstimesml@gmail.com>
2025-12-17 13:24:46 +0100

    resolved login issuees

--- models/user.js
+++ models/user.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     unique: true,
     trim: true,
     lowercase: true,
-    match: [/^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3})+$/, 'Please fill a valid email address']
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
   password: {
     type: String,
