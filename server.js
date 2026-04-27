dd5755bcd13031c361cf667bb2410a372510989c
samocology <godstimesml@gmail.com>
2026-04-27 12:41:21 +0100

    added CORS

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const connectDB = require('./db');
+const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
 
